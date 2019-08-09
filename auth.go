b34a9fa13e3b4a05fa59cd59a621bb65004912e4
Zahid Al Tair <mdzahidh119@gmail.com>
2019-08-09 16:32:51 +0600

    added jwt generate & validate token

---
+++ auth.go
@@ -0,0 +1,50 @@
+package gauth
+
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
+)
+
+// Auth ...
+type Auth struct {
+	method        jwt.SigningMethod
+	expireTimeout time.Duration
+	secret        string
+}
+
+// NewAuth ...
+func NewAuth(secret string, exp time.Duration) *Auth {
+	return &Auth{
+		method:        jwt.SigningMethodHS256,
+		expireTimeout: exp,
+		secret:        secret,
+	}
+}
+
+// GenerateToken ...
+func (auth *Auth) GenerateToken(sub string) (string, error) {
+	id, _ := uuid.NewUUID()
+	return jwt.NewWithClaims(auth.method, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(auth.expireTimeout).Unix(),
+		Id:        id.String(),
+		IssuedAt:  time.Now().Unix(),
+		Subject:   sub,
+	}).SignedString([]byte(auth.secret))
+}
+
+func (auth *Auth) parser(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected Signing Method: %v", token.Header["alg"])
+	}
+	return []byte(auth.secret), nil
+}
+
+// Validate ...
+func (auth *Auth) Validate(tokenString string) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, auth.parser)
+	return claims, err
+}
---
+++ auth_test.go
@@ -0,0 +1,36 @@
+package gauth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuth(t *testing.T) {
+	auth := NewAuth("secret", 1*time.Second)
+	token, err := auth.GenerateToken("user1")
+
+	t.Run("generate token", func(t *testing.T) {
+		assert.NoError(t, err)
+		assert.NotEmpty(t, token)
+	})
+
+	t.Run("validate token", func(t *testing.T) {
+		claims, err := auth.Validate(token)
+		assert.NoError(t, err)
+		assert.Equal(t, "user1", claims["sub"])
+	})
+
+	t.Run("expired token", func(t *testing.T) {
+		time.Sleep(2 * time.Second)
+		_, err := auth.Validate(token)
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid token", func(t *testing.T) {
+		token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+		_, err := auth.Validate(token)
+		assert.Error(t, err)
+	})
+}
